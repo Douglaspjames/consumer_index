70b3f1923a3694b93326b5ac05ae31326dd13faa
Eleanor RooseveltDoug <dpjames29@gmail.com>
2017-03-05 22:14:28 -0600

    wrote some tests

--- lib/consumer_index.rb
+++ lib/consumer_index.rb
@@ -1,11 +1,36 @@
 require "consumer_index/version"
 require "unirest"
 
+
+
 module ConsumerIndex
   class ScoringAdvice
-    def initialize(consumer_data)
-      @propensity = consumer_data["propensity"].to_f
-      @ranking = consumer_data["ranking"]
+    attr_accessor :propensity, :ranking
+
+    def initialize(index)
+      @propensity = index[:propensity].to_f
+      @ranking = index[:ranking]
     end
+
+    def self.request(data)
+      income = data[:income].to_s
+      zip_code = data[:zip_code].to_s
+      age = data[:age].to_s
+
+      response = Unirest.get("http://not_real.com/customer_scoring",
+        headers:{ "Accept" => "application/json" },
+        parameters: { 
+          income: income,
+          zip_code: zip_code,
+          age: age 
+          }).body
+
+      ScoringAdvice.new(response)
+
+    end
+
+
+
+
   end
 end
--- spec/consumer_index_spec.rb
+++ spec/consumer_index_spec.rb
@@ -1,11 +1,42 @@
 require "spec_helper"
 
+
+CISA = ConsumerIndex::ScoringAdvice
+
 describe ConsumerIndex do
   it "has a version number" do
     expect(ConsumerIndex::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '#self.request' do
+    context "upon initialization" do
+      it "is a ScoringAdvice object" do
+        response = { 
+          "propensity": 0.26532,
+          "ranking": "C" }
+        index = ConsumerIndex::ScoringAdvice.new(response)
+        expect(index.class).to eq(    ConsumerIndex::ScoringAdvice)
+      end
+     
+  
+
+
+      # it "has a propensity" do 
+      #   expect(index.propensity).to eq( 0  .26532)
+      #   end
+  
+      it "has a ranking" do 
+        response = { 
+          "propensity": 0.26532,
+          "ranking": "C" }
+        index = ConsumerIndex::ScoringAdvice.new(propensity: 0.26532,
+          ranking: "C")
+        expect(index.ranking).to eq("C")
+        end
+
+    end
   end
+
+
+
 end
